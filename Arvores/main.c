e666b23a4ceec3c37a487e492963e13e4e729b18
Bruno <bruno.hp2002@aluno.ifsc.edu.br>
2023-12-08 15:25:14 -0300

    jknbijk

---
+++ Arvores/main.c
@@ -0,0 +1,12 @@
+#include "Arvore.h"
+
+int main () {
+    char teste[10] = {'f', '.', 'p', 'i', 'n', 'g', 'o', 0, 0, 0};
+
+    for (int i = 0; i < 10; ++i) {
+        printf("%d\n", teste[i]);
+    }
+
+
+    return 0;
+}
